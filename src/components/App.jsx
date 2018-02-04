d3b28ebfc03d6de9d3edcca8ac231f63743c5171
Adam Bukowski <bukowski.adam@gmail.com>
2018-02-04 12:52:11 +0100

    add github corner with link to repo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './App.css';
 import Board2d from './Board2d';
+import GithubCorner from './GithubCorner';
 
 const size = 32;
 
@@ -9,6 +10,7 @@ const App = () => (
     <div className="App">
         <header className="AppHeader">
             <h1>React Animations!</h1>
+            <GithubCorner url="https://github.com/bukowskiadam/react-animations" color="#222222" fill="#ffffff" />
         </header>
         <div className="AppContainer">
             <h3>Click on board!</h3>
